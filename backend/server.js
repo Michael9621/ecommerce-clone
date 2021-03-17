2fdc9d1d03ad7757add7ab2eb471e973fe78181c
Michael9621 <oandami@gmail.com>
2021-03-17 11:16:19 +0300

    create sample users in mongo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import data from './data.js';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+mongoose.connect('mongodb+srv://cristiano:ombati1995@cluster0.tunda.mongodb.net/amazona?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -14,6 +22,12 @@ app.get('/api/products/:id', (req, res) => {
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
+});
+
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 app.get('/', (req, res) => {
