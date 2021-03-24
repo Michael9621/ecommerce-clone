671a452bb422740a3e0411ead6066fa7253c8f66
Michael9621 <oandami@gmail.com>
2021-03-24 18:19:58 +0300

    sign in action

--- amazonclone/src/store.js
+++ amazonclone/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
 import {
     productDetailsReducer,
@@ -8,6 +9,11 @@ import {
   } from './reducers/productReducers';
 
   const initialState = {
+    userSignin: {
+      userInfo: localStorage.getItem('userInfo')
+        ? JSON.parse(localStorage.getItem('userInfo'))
+        : null,
+    },
     cart: {
       cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
@@ -19,6 +25,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
