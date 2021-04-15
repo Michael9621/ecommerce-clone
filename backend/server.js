58fa95fc4b467b4ea5d9dc0abafd49f805a5f505
Michael9621 <oandami@gmail.com>
2021-04-15 22:19:16 +0300

    env & pay pal button

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,32 @@
 import express from 'express'
+
 import data from './data.js';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
-import dotenv from 'dotenv';
+import env from 'dotenv'
+
 import productRouter from './routers/productRouter.js';
 import orderRouter from './routers/orderRouter.js';
+//require('dotenv').config({ path:'.env'})
 
-dotenv.config();
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://cristiano:ombati1995@cluster0.tunda.mongodb.net/amazona?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://cristiano:ombati1995@cluster0.tunda.mongodb.net/amazona?retryWrites=true&w=majority', 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
 app.use('/api/products', productRouter);
+
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 
 app.use('/api/users', userRouter);
 
